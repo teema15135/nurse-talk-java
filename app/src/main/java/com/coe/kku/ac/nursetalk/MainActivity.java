bc6b692e25749ca4b47f9637722113d33a1568b7
chompusama <chadaporn.p@kkumai.com>
2019-10-02 20:47:37 +0700

    add stupid button

--- app/src/main/java/com/coe/kku/ac/nursetalk/MainActivity.java
+++ app/src/main/java/com/coe/kku/ac/nursetalk/MainActivity.java
@@ -2,13 +2,27 @@ package com.coe.kku.ac.nursetalk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button vocabBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        vocabBtn = (Button)findViewById(R.id.vocab_btn);
+
+        vocabBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, VocabActivity.class));
+            }
+        });
     }
 }
