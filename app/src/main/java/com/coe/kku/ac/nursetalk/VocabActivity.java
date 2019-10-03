71ecc95cda509199077cbed34a5ed5762a69d47a
chompusama <chadaporn.p@kkumai.com>
2019-10-04 01:10:52 +0700

    add testActivity for teema

--- app/src/main/java/com/coe/kku/ac/nursetalk/VocabActivity.java
+++ app/src/main/java/com/coe/kku/ac/nursetalk/VocabActivity.java
@@ -1,5 +1,6 @@
 package com.coe.kku.ac.nursetalk;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -10,6 +11,7 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import java.util.Locale;
 
@@ -19,6 +21,7 @@ public class VocabActivity extends AppCompatActivity implements View.OnClickList
     public final static int SYMPTOM = 1;
 
     private Button thaiBtn, engBtn;
+    private ImageButton diseaseBtn;
     private Vocabulary vocabInstance;
     private int type;
 
@@ -28,6 +31,14 @@ public class VocabActivity extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_vocab);
 
 
+        diseaseBtn = (ImageButton)findViewById(R.id.diseaseImgButton);
+
+        diseaseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(VocabActivity.this, TestActivity.class));
+            }
+        });
 
     }
 
