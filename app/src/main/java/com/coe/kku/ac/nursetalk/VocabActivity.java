e6997ad831a75d5fff15780fa19dfbc79c56a02a
Teema Luksameset <teema.ma.luk@kkumail.com>
2019-10-04 01:51:39 +0700

    Formatting

--- app/src/main/java/com/coe/kku/ac/nursetalk/VocabActivity.java
+++ app/src/main/java/com/coe/kku/ac/nursetalk/VocabActivity.java
@@ -3,17 +3,10 @@ package com.coe.kku.ac.nursetalk;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
-
-import java.util.Locale;
 
 public class VocabActivity extends AppCompatActivity implements View.OnClickListener {
 
