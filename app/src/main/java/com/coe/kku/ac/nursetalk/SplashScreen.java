176c8a8ea5b635c7175b9c59d1dde20d24e173d8
Teema Luksameset <teema.ma.luk@kkumail.com>
2019-10-08 23:47:04 +0700

    Remove splash slide transition

--- app/src/main/java/com/coe/kku/ac/nursetalk/SplashScreen.java
+++ app/src/main/java/com/coe/kku/ac/nursetalk/SplashScreen.java
@@ -34,6 +34,7 @@ public class SplashScreen extends AppCompatActivity {
             public void run() {
                 Intent intent = new Intent(SplashScreen.this, MainActivity.class);
                 startActivity(intent);
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
                 finish();
             }
         };
