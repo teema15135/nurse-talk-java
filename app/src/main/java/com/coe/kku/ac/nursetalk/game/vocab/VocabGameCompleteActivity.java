c9f6e605057ec4f5a3249d75dc0f3fe134a75da4
Teema Luksameset <teema.ma.luk@kkumail.com>
2019-10-17 02:46:56 +0700

    Have trophy and score show after complete vocab game

--- app/src/main/java/com/coe/kku/ac/nursetalk/game/vocab/VocabGameCompleteActivity.java
+++ app/src/main/java/com/coe/kku/ac/nursetalk/game/vocab/VocabGameCompleteActivity.java
@@ -3,6 +3,7 @@ package com.coe.kku.ac.nursetalk.game.vocab;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.coe.kku.ac.nursetalk.R;
@@ -10,6 +11,7 @@ import com.coe.kku.ac.nursetalk.R;
 public class VocabGameCompleteActivity extends AppCompatActivity {
 
     private TextView vocabScoreTextView;
+    private ImageView scoreMessageImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +21,18 @@ public class VocabGameCompleteActivity extends AppCompatActivity {
         int score = getIntent().getIntExtra("score", -1);
 
         vocabScoreTextView = (TextView) findViewById(R.id.vocab_score_complete_text_view);
+        scoreMessageImage = (ImageView) findViewById(R.id.score_message_image_view);
 
         vocabScoreTextView.setText(Integer.toString(score));
+
+        if (score < 4) {  // Score 0 - 3
+            scoreMessageImage.setImageResource(R.drawable.score_zero_to_three_message);
+        } else if (score < 6) {  // Score 4 - 5
+            scoreMessageImage.setImageResource(R.drawable.score_four_to_five_message);
+        } else if (score < 9) {  // Score 6 - 8
+            scoreMessageImage.setImageResource(R.drawable.score_six_to_eight_message);
+        } else {  // Score 9 - 10
+            scoreMessageImage.setImageResource(R.drawable.score_nine_to_ten_message);
+        }
     }
 }
